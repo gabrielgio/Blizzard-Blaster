a4e19b39861f7e75ded145c627df4778f4b816d1
Gabriel <gabrielg.desouza@gmail.com>
2015-02-02 18:05:12 -0800

    Added libGdx template project

---
+++ desktop/src/com/blizzardBlaster/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.blizzardBlaster.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.blizzardBlaster.game.BlizzardBlaster;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlizzardBlaster(), config);
+	}
+}
