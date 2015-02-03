d25081a18d2a0bcd54b21028b34f4b3de3b82181
Gabriel <gabrielg.desouza@gmail.com>
2015-02-02 18:23:48 -0800

    World set up

--- core/src/com/blizzardBlaster/game/BlizzardBlaster.java
+++ core/src/com/blizzardBlaster/game/BlizzardBlaster.java
@@ -3,25 +3,28 @@ package com.blizzardBlaster.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class BlizzardBlaster extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+    private SpriteBatch batch;
+    World world;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        batch = new SpriteBatch();
+        world = new World(new Vector2(0, -98f), true);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.end();
 	}
 }
